4fb7a2f5d8fe87883e4e55a96f0a332d8d0e5a30
Borges10002 <borges10002@gmail.com>
2023-03-28 23:30:30 -0300

    criando service de criacao de usuarios

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,29 @@
+import "reflect-metadata";
+
 import express from "express";
 import path from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/rocketsocket", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions)
+  .then((res) => {
+    console.log("Connected to Distribution API Database - Initial Connection");
+  })
+  .catch((err) => {
+    console.log(
+      `Initial Distribution API Database connection error occured -`,
+      err
+    );
+  });
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
---
+++ src/schemas/Message.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+type Message = Document & {
+  to: String;
+  text: String;
+  created_at: Date;
+  roomId: String;
+};
+
+const MessageSchema = new Schema({
+  to: {
+    type: Schema.Types.ObjectId,
+  },
+  text: String,
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  roomId: {
+    type: String,
+    ref: "ChatRoom",
+  },
+});
+
+const Message = mongoose.model<Message>("Messages", MessageSchema);
+
+export { Message };
